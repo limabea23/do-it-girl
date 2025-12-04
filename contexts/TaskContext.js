effee0ebab70807706a79051af448044ba84b505
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-12-04 16:21:14 -0300

    feat: update TaskContext.js

--- contexts/TaskContext.js
+++ contexts/TaskContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useCallback, useContext, useMemo, useState } from 'react'
+import React, { createContext, useCallback, useContext, useEffect, useMemo, useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const TaskContext = createContext(null)
 
